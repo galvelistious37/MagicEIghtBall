6acab03a3d2466dcb3c243044790ad0838419c64
galvelistious37 <galvelistious37@gmail.com>
2020-03-13 18:06:48 -0700

    Desktop refactor

--- src/com/laptop/stuff/Answers.java
+++ src/com/laptop/stuff/Answers.java
@@ -12,20 +12,40 @@ public class Answers {
 
     private Map<Integer, String[]> buildEightBallAnswers(){
         Map<Integer, String[]> createAnswers = new HashMap<Integer, String[]>();
-        createAnswers.put(1, "Hell Nah".split(" "));
-        createAnswers.put(2, "Nope, don't even plan on it".split(" "));
-        createAnswers.put(3, "Yeah... and by yeah, I mean no".split(" "));
-        createAnswers.put(4, "Eh... Maybe".split(" "));
-        createAnswers.put(5, "Ask me when I'm not drunk".split(" "));
-        createAnswers.put(6, "I don't know".split(" "));
-        createAnswers.put(7, "Yup".split(" "));
-        createAnswers.put(8, "Hell Yeeeeaaaaahhhhh".split(" "));
-        createAnswers.put(9, "Yup yup yup".split(" "));
-        createAnswers.put(10, "Only if Ruby says yes".split(" "));
+        createAnswers.putAll(populateShittyAnswers());
+        createAnswers.putAll(populateNeutralAnswers());
+        createAnswers.putAll(populateGoodAnswes());
         return createAnswers;
     }
 
     public Map<Integer, String[]> getEightBallAnswers() {
         return eightBallAnswers;
+    }
+
+    private Map<Integer, String[]> populateShittyAnswers(){
+        Map<Integer, String[]> createAnswers = new HashMap<Integer, String[]>();
+        createAnswers.put(1, "Hell Nah".split(" "));
+        createAnswers.put(2, "Nope, don't even plan on it".split(" "));
+        createAnswers.put(3, "Yeah... and by yeah, I mean no".split(" "));
+        return createAnswers;
+    }
+
+    private Map<Integer, String[]> populateNeutralAnswers(){
+        Map<Integer, String[]> createAnswers = new HashMap<Integer, String[]>();
+        createAnswers.put(4, "Eh... Maybe".split(" "));
+        createAnswers.put(5, "Ask me when I'm not drunk".split(" "));
+        createAnswers.put(6, "I don't know".split(" "));
+        return createAnswers;
+    }
+
+    private Map<Integer, String[]> populateGoodAnswes(){
+        Map<Integer, String[]> createAnswers = new HashMap<Integer, String[]>();
+        createAnswers.put(7, "Yup".split(" "));
+        createAnswers.put(8, "Hell Yeeeeaaaaahhhhh".split(" "));
+        createAnswers.put(9, "Yup yup yup".split(" "));
+        createAnswers.put(10, "Only if Ruby says yes".split(" "));
+        createAnswers.put(11, "Fo' Sho' Baws".split(" "));
+        createAnswers.put(12, "Myeah".split(" "));
+        return createAnswers;
     }
 }
